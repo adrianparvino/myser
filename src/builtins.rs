802d8979e9fbaae2d80007ed61d59f566756b7b2
Adrian Parvin Ouano <adrianparvino@gmail.com>
2022-07-21 18:09:19 +0800

    Add additional builtins and builtins floating point support

--- src/builtins.rs
+++ src/builtins.rs
@@ -2,18 +2,38 @@ use crate::{pool::{Pool, RcValue}, value::Value};
 use core::ops::Deref;
 use heapless::FnvIndexMap;
 
-pub fn add<'s, Context, const N: usize>(_: &mut Context, pool: &'s Pool<'s, N>, mut args: RcValue<'s>) -> RcValue<'s> {
-    let mut result = 0;
+pub fn add<'s, Context, const N: usize>(_: &mut Context, pool: &'s Pool<'s, N>, args: RcValue<'s>) -> RcValue<'s> {
+    let mut args = args.deref();
+    let mut result = Value::Integer(0);
 
     loop {
-        match args.deref() {
+        match args {
             Value::Cons(car, cdr) => {
-                if let Value::Integer(car) = car.deref() {
-                    result += car;
-                    args = cdr.clone();
-                }
+                result = match (car.deref(), result) {
+                    (Value::Integer(car), Value::Integer(n)) => {
+                        Value::Integer(car + n)
+                    },
+                    (Value::Number(car), Value::Integer(n)) => {
+                        Value::Number(car + n as f64)
+                    },
+                    (Value::Integer(car), Value::Number(n)) => {
+                        Value::Number(*car as f64 + n)
+                    },
+                    (Value::Number(car), Value::Number(n)) => {
+                        Value::Number(car + n)
+                    }
+                    _ => {
+                        panic!()
+                    }
+                };
+
+                args = &cdr;
             },
-            Value::Symbol("nil") => { return pool.new_integer(result); },
+            Value::Symbol("nil") => { return match result {
+                Value::Integer(result) => pool.new_integer(result),
+                Value::Number(result) => pool.new_number(result),
+                _ => panic!()
+            }},
             _ => {
                 return pool.new_symbol("nil");
             }
@@ -24,32 +44,161 @@ pub fn add<'s, Context, const N: usize>(_: &mut Context, pool: &'s Pool<'s, N>, 
 pub fn sub<'s, Context, const N: usize>(_: &mut Context, pool: &'s Pool<'s, N>, args: RcValue<'s>) -> RcValue<'s> {
     match args.deref() {
         Value::Cons(car, args) => {
-            if let Value::Integer(car) = car.deref() {
-                if let  Value::Symbol("nil") = args.deref() {
-                    return pool.new_integer(-car);
-                } else {
-                    let mut args = args;
-                    let mut result = *car;
-
-                    loop {
-                        match args.deref() {
-                            Value::Cons(car, cdr) => {
-                                if let Value::Integer(car) = car.deref() {
-                                    result -= car;
-                                    args = cdr;
-                                } else {
-                                    return pool.new_integer(result);
-                                }
-                            },
-                            Value::Symbol("nil") => { return pool.new_integer(result); },
-                            _ => {
-                                return pool.new_symbol("nil");
-                            }
-                        }
+            if let Value::Symbol("nil") = args.deref() {
+                match car.deref() {
+                    Value::Integer(n) => {
+                        return pool.new_integer(-n);
+                    },
+                    Value::Number(x) => {
+                        return pool.new_number(-x);
+                    },
+                    _ => {
+                        return pool.new_symbol("nil");
                     }
                 }
             }
-            pool.new_integer(0)
+
+            let mut result = match car.deref() {
+                Value::Integer(n) =>  Value::Integer(*n),
+                Value::Number(x) =>  Value::Number(*x),
+                _ => panic!()
+            };
+            let mut args = args.deref();
+
+            loop {
+                match args {
+                    Value::Cons(car, cdr) => {
+                        result = match (car.deref(), result) {
+                            (Value::Integer(car), Value::Integer(n)) => {
+                                Value::Integer(n - car)
+                            },
+                            (Value::Number(car), Value::Integer(n)) => {
+                                Value::Number(n as f64 - car)
+                            },
+                            (Value::Integer(car), Value::Number(n)) => {
+                                Value::Number(n - *car as f64)
+                            },
+                            (Value::Number(car), Value::Number(n)) => {
+                                Value::Number(n - car)
+                            }
+                            _ => {
+                                panic!()
+                            }
+                        };
+
+                        args = &cdr;
+                    },
+                    Value::Symbol("nil") => { return match result {
+                        Value::Integer(result) => pool.new_integer(result),
+                        Value::Number(result) => pool.new_number(result),
+                        _ => panic!()
+                    }},
+                    _ => {
+                        return pool.new_symbol("nil");
+                    }
+                }
+            }
+        },
+        _ => pool.new_integer(0)
+    }
+}
+
+pub fn times<'s, Context, const N: usize>(_: &mut Context, pool: &'s Pool<'s, N>, args: RcValue<'s>) -> RcValue<'s> {
+    let mut args = args.deref();
+    let mut result = Value::Integer(1);
+
+    loop {
+        match args {
+            Value::Cons(car, cdr) => {
+                result = match (car.deref(), result) {
+                    (Value::Integer(car), Value::Integer(n)) => {
+                        Value::Integer(car * n)
+                    },
+                    (Value::Number(car), Value::Integer(n)) => {
+                        Value::Number(car * n as f64)
+                    },
+                    (Value::Integer(car), Value::Number(n)) => {
+                        Value::Number(*car as f64 * n)
+                    },
+                    (Value::Number(car), Value::Number(n)) => {
+                        Value::Number(car * n)
+                    }
+                    _ => {
+                        panic!()
+                    }
+                };
+
+                args = &cdr;
+            },
+            Value::Symbol("nil") => { return match result {
+                Value::Integer(result) => pool.new_integer(result),
+                Value::Number(result) => pool.new_number(result),
+                _ => panic!()
+            }},
+            _ => {
+                return pool.new_symbol("nil");
+            }
+        }
+    }
+}
+
+pub fn div<'s, Context, const N: usize>(_: &mut Context, pool: &'s Pool<'s, N>, args: RcValue<'s>) -> RcValue<'s> {
+    match args.deref() {
+        Value::Cons(car, args) => {
+            if let Value::Symbol("nil") = args.deref() {
+                match car.deref() {
+                    Value::Integer(_) => {
+                        return pool.new_integer(0);
+                    },
+                    Value::Number(x) => {
+                        return pool.new_number(1.0/x);
+                    },
+                    _ => {
+                        return pool.new_symbol("nil");
+                    }
+                }
+            }
+
+            let mut result = match car.deref() {
+                Value::Integer(n) =>  Value::Integer(*n),
+                Value::Number(x) =>  Value::Number(*x),
+                _ => panic!()
+            };
+            let mut args = args.deref();
+
+            loop {
+                match args {
+                    Value::Cons(car, cdr) => {
+                        result = match (car.deref(), result) {
+                            (Value::Integer(car), Value::Integer(n)) => {
+                                Value::Integer(n / car)
+                            },
+                            (Value::Number(car), Value::Integer(n)) => {
+                                Value::Number(n as f64 / car)
+                            },
+                            (Value::Integer(car), Value::Number(n)) => {
+                                Value::Number(n / *car as f64)
+                            },
+                            (Value::Number(car), Value::Number(n)) => {
+                                Value::Number(n / car)
+                            }
+                            _ => {
+                                panic!()
+                            }
+                        };
+
+                        args = &cdr;
+                    },
+                    Value::Symbol("nil") => { return match result {
+                        Value::Integer(result) => pool.new_integer(result),
+                        Value::Number(result) => pool.new_number(result),
+                        _ => panic!()
+                    }},
+                    _ => {
+                        return pool.new_symbol("nil");
+                    }
+                }
+            }
         },
         _ => pool.new_integer(0)
     }
@@ -67,6 +216,8 @@ impl <'s, Context, const N: usize, const BUILTINS: usize> Builtins<'s, Context, 
         let mut this = Self { map };
         this.add("+", add);
         this.add("-", sub);
+        this.add("*", times);
+        this.add("/", div);
 
         return this;
     }
